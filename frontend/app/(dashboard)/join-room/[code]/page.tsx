50707ee28641ba05686a6d392789f25c52b89690
Anmol walia <anmolwalia025@gmail.com>
2025-08-06 00:37:39 +0530

    room model

--- frontend/app/(dashboard)/join-room/[code]/page.tsx
+++ frontend/app/(dashboard)/join-room/[code]/page.tsx
@@ -1,10 +1,10 @@
 import Room from "@/app/Components/Room";
 import axios from "axios";
 
-export default async function Page({ params }: { params: { code: string } }) {
-  const { code } = params;
+export default async function Page({ params }: { params: Promise<{ code: string }> }) {
+  const { code } = await params; 
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_Url}/api/room/${code}`);
+    await axios.get(`${process.env.NEXT_PUBLIC_Url}/api/room/${code}`);
     return <Room roomId={code} />;
   } catch (error) {
     return <div className="w-full flex items-center justify-center text-red-700 text-2xl">Room not found or error creating room.</div>;
